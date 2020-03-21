5ce746100559a1f2490577ac9d190839e384af50
Sccoupe <46354555+Sccoupe@users.noreply.github.com>
2020-03-21 11:19:32 -0400

    Update driver_monitor.py

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -12,7 +12,7 @@ if not travis:
 else:
   awareness_factor = 1
 
-_AWARENESS_TIME = 70. * awareness_factor  # one minute limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_TIME = 600. * awareness_factor  # one minute limit without user touching steering wheels make the car enter a terminal status
 _AWARENESS_PRE_TIME_TILL_TERMINAL = 15.  # a first alert is issued 25s before expiration
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 15s before start decelerating the car
 _DISTRACTED_TIME = 11.
